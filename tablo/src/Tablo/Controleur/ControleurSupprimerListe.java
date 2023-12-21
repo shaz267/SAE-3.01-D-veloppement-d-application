3fc0ad09ffb74fa565a839b0a29d9622ec7f1c1d
Math.dlcrt <mathias.delcourt4@etu.univ-lorraine.fr>
2023-12-21 22:38:40 +0100

    Réparation problème suppression Liste et Tâche

--- tablo/src/Tablo/Controleur/ControleurSupprimerListe.java
+++ tablo/src/Tablo/Controleur/ControleurSupprimerListe.java
@@ -27,5 +27,6 @@ public class ControleurSupprimerListe implements EventHandler<MouseEvent> {
 
 		//On retire la liste courante
 		modele.retirerListe(modele.getListes().get(numListe - 1));
+		modele.changerTacheCourante(numListe-1);
 	}
 }

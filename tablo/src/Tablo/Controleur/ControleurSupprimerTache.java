133857e769fe8c13dcbe9baeb36d3d9297f70f25
Math.dlcrt <mathias.delcourt4@etu.univ-lorraine.fr>
2023-12-21 11:55:15 +0100

    Controleurs pour modifierUneTache

---
+++ tablo/src/Tablo/Controleur/ControleurSupprimerTache.java
@@ -0,0 +1,31 @@
+package Tablo.Controleur;
+
+import Tablo.Modele.Modele;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class ControleurSupprimerTache implements EventHandler<MouseEvent> {
+
+	/**
+	 * Modèle de l'application
+	 */
+	private Modele modele;
+
+	public ControleurSupprimerTache(Modele m){ this.modele = m; }
+
+	@Override
+	public void handle(MouseEvent mouseEvent) {
+
+		// On récupère le bouton supprimer
+		Button supprimer = (Button) mouseEvent.getSource();
+		// On le désactive
+		supprimer.setDisable(true);
+
+		//On récupère le numéro de la liste qui a été cliquée
+		int numTache = Modele.getTacheCourante();
+
+		//On retire la liste courante
+		modele.retirerListe(modele.getListes().get(numTache - 1));
+	}
+}
---
+++ tablo/src/Tablo/Controleur/ControleurTacheClicDroit.java
@@ -0,0 +1,97 @@
+package Tablo.Controleur;
+
+import Tablo.Loggeur;
+import Tablo.Modele.Modele;
+import Tablo.Vue.VueTache;
+import javafx.event.EventHandler;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
+public class ControleurTacheClicDroit implements EventHandler<MouseEvent> {
+
+	/**
+	 * Modele de l'application
+	 */
+	private Modele modele;
+
+	/**
+	 * Constructeur de la classe ControleurTacheClicDroit
+	 * @param m Modele de l'application
+	 */
+	public ControleurTacheClicDroit(Modele m){ this.modele = m; }
+
+	/**
+	 * Méthode de la classe EventHandler qui permet de gérer la modification d'une tache
+	 * @param mouseEvent
+	 */
+	@Override
+	public void handle(MouseEvent mouseEvent) {
+
+		// Si le clique effectué est un clique droit alors
+		if(mouseEvent.getButton().equals(MouseButton.SECONDARY)){
+
+			// On récupère la tâche sur laquelle on a cliqué
+			VueTache vt = (VueTache) mouseEvent.getSource();
+
+			// On récupère le numéro de la tâche
+			int numTache = vt.getNumTache();
+
+			// On change la tâche courante
+			modele.changerTacheCourante(numTache);
+
+			//On créé la boîte de dialogue
+			ChoiceDialog<VBox> dialog = new ChoiceDialog<>();
+			dialog.setTitle("Modifier Tâche");
+			dialog.setHeaderText("Modifier une tâche");
+
+			// On crée une VBox qui va contenir les choix de l'utilisateur
+			VBox conteneur = new VBox();
+
+			// On crée le TextField qui va contenir le nouveau titre de la tâche
+			TextField titreField = new TextField();
+
+			// On crée le TextArea qui va contenir la description de la tâche
+			TextArea contenuArea = new TextArea();
+
+			// On crée le DatePicker pour gérer la date de début
+			DatePicker dateDeb = new DatePicker();
+			dateDeb.setShowWeekNumbers(false);
+
+			// On crée le DatePicker pour gérer la date de fin
+			DatePicker dateFin = new DatePicker();
+			dateDeb.setShowWeekNumbers(false);
+
+			// On crée la comboBox qui contiendra la liste des tâches qu'on pourra
+
+			// On crée le bouton supprimer pour supprimer la liste
+			Button supprButton = new Button("Supprimer");
+			// On ajoute le controleur pour supprimer la liste
+			supprButton.setOnMouseClicked(new ControleurSupprimerTache(this.modele));
+
+			// On ajoute les composantes graphiques à la VBox
+			conteneur.getChildren().addAll(titreField, contenuArea, dateDeb, dateFin, supprButton);
+
+			// On ajoute la VBox à la boîte de dialogue
+			dialog.getDialogPane().setContent(conteneur);
+
+			// Affichage de la boîte de dialogue et attente de la réponse de l'utilisateur
+			dialog.showAndWait();
+
+			// Si le titre n'est pas vide
+			if(titreField.getText() != ""){
+				// On récupère le nouveau titre de la tâche
+				String titre = titreField.getText();
+				this.modele.getTaches().get(Modele.getTacheCourante() - 1).changerTitre(titre);
+
+				// On notifie les observateurs
+				this.modele.notifierObservateurs();
+				// On enregistre l'action dans les logs
+				Loggeur.enregistrer("Modification de titre d'une tâche");
+			}
+			// Si le contenu n'est pas vide
+			if()
+		}
+	}
+}

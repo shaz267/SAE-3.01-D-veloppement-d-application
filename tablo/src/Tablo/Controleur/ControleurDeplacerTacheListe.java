2159d8088a97b7c2a270146ce2cd47c9eb255eb5
ismaelkone1 <ismael.kone4@etu.univ-lorraine.fr>
2024-01-09 13:57:10 +0100

    Deplacement de tache, MAJ des controleurs

--- tablo/src/Tablo/Controleur/ControleurDeplacerTacheListe.java
+++ tablo/src/Tablo/Controleur/ControleurDeplacerTacheListe.java
@@ -12,6 +12,8 @@ public class ControleurDeplacerTacheListe implements EventHandler<DragEvent> {
     private Modele modele;
     private VueListe vueListeDestination;
 
+    private Tache tacheADeplacer;
+
     public ControleurDeplacerTacheListe(Modele m) {
         this.modele = m;
     }
@@ -26,48 +28,89 @@ public class ControleurDeplacerTacheListe implements EventHandler<DragEvent> {
         }
         else if (dragEvent.getEventType() == DragEvent.DRAG_EXITED)
         {
-            this.ListeDragExited(dragEvent);
+            this.listeDragExited(dragEvent);
         }
         else if (dragEvent.getEventType() == DragEvent.DRAG_DROPPED)
         {
-            this.ListeDragDropped(dragEvent);
+            this.listeDragDropped(dragEvent);
         }
+        else
+
         System.out.println("DragEvent : " + dragEvent.getEventType());
 
     }
 
-    private void listeDragOver(DragEvent dragEvent)
-    {
-        if (dragEvent.getGestureSource() != dragEvent.getTarget() && dragEvent.getDragboard().hasString())
-        {
+    private boolean listeDragOver(DragEvent dragEvent) {
+        boolean accept = false;
+        if (dragEvent.getGestureSource() != dragEvent.getTarget() && dragEvent.getDragboard().hasString()) {
+            System.out.println("Drag Over - Source: " + dragEvent.getGestureSource() + ", Target: " + dragEvent.getTarget());
             dragEvent.acceptTransferModes(TransferMode.COPY_OR_MOVE);
-            //Modele.setListeDestination(this.vueListeDestination.getNumListe());
+            this.vueListeDestination = (VueListe) dragEvent.getTarget();
+            accept = true;
         }
+        dragEvent.consume();
+        return accept;
+    }
+
+    // On drag dropped on source
+    private void listeDragDropped(DragEvent dragEvent){
+        this.vueListeDestination = (VueListe) dragEvent.getTarget();
+        System.out.println("Drag Dropped - Source: " + dragEvent.getGestureSource() + ", Target: " + dragEvent.getTarget());        // On récupère la liste de destination
+        int numListe = this.vueListeDestination.getNumListe();
+        System.out.println("numListe : " + numListe);
+
+        // On vérifie que le numéro de la liste est valide
+        if (numListe >= 0 && numListe < modele.getListes().size()) {
+            System.out.println("deplacement de la tache");
+            this.tacheADeplacer = getTacheADeplacer();
+            int numTache = Modele.getTacheCourante();
+            System.out.println("numTache : " + Modele.getTacheCourante());
+            // On vérifie que le numéro de la tache est valide
+                System.out.println("tache valide");
+                Tache tache = modele.getTaches().get(Modele.getTacheCourante());
+                modele.deplacerTache(tache, numListe);
+                System.out.println("Tache déplacée " + tache.getTitre() + " dans la liste " + numListe);
+                System.out.println(this.modele.getListes().get(numListe).getTaches().get(numTache).getTitre());
+
+        }
+
+        dragEvent.setDropCompleted(true);
         dragEvent.consume();
     }
 
-    private void ListeDragExited(DragEvent dragEvent)
-    {
-        if (!dragEvent.isAccepted())
-        {
-            this.vueListeDestination = (VueListe) dragEvent.getTarget();
-            //this.vueListeDestination.setStyle("-fx-background-color: #f4a8f6;");
-        }
+    private void listeDragExited(DragEvent dragEvent) {
+        this.vueListeDestination = (VueListe) dragEvent.getTarget();
+        System.out.println("Drag Dropped - Source: " + dragEvent.getGestureSource() + ", Target: " + dragEvent.getTarget());        // On récupère la liste de destination
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //dragEvent.setDropCompleted(true);
         dragEvent.consume();
     }
 
-    private void ListeDragDropped(DragEvent dragEvent)
-    {
-        boolean success = false;
-        int numTache = ControleurDeplacerTache.getVueTacheADeplacer().getNumTache();
-            Tache tache = this.modele.getTaches().get(numTache);
-            //this.modele.retirerTache(tache);
-            this.vueListeDestination = (VueListe) dragEvent.getTarget();
-            success = true;
-
-        dragEvent.setDropCompleted(success);
-        dragEvent.consume();
-        System.out.println("DragEvent : " + dragEvent.getEventType());
+    public Tache getTacheADeplacer() {
+        Tache tache = null;
+        for (int i = 0; i < modele.getTaches().size(); i++) {
+            if (modele.getTaches().get(i).getNumTache() == Modele.getTacheCourante()) {
+                tache = modele.getTaches().get(i);
+            }
+        }
+        return tache;
     }
 
 
